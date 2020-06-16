7210762ab4f16d009a694faef8dc8f13f53f1b1a
LucDelvolve <31876700+LucDelvolve@users.noreply.github.com>
2020-06-16 12:21:51 +0200

    Add files via upload

---
+++ pwd/config.js
@@ -0,0 +1,13 @@
+((window) => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyA71VrHMK4Tnj6QG5xQ29CFN2-c5laN7PY",
+      authDomain: "iw3-firechat.firebaseapp.com",
+      databaseURL: "https://iw3-firechat.firebaseio.com",
+      projectId: "iw3-firechat",
+      storageBucket: "iw3-firechat.appspot.com",
+      messagingSenderId: "495780864713",
+      appId: "1:495780864713:web:1851cb8c23c3d0d64ea318"
+    };
+  
+    window.config = firebaseConfig;
+  })(window);
